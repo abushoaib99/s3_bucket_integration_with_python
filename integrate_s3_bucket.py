2b3fdde00f7325b70530eaf7fc1ebd721ba91c0d
abushoaib99 <abushoaib99@gmail.com>
2024-03-18 19:48:43 +0600

    - create class

--- integrate_s3_bucket.py
+++ integrate_s3_bucket.py
@@ -18,63 +18,60 @@ s3_client = boto3.client(
 )
 
 
-# Function to upload a file to S3 bucket
-def upload_file_to_s3(file_path, bucket_name, object_name):
-    try:
-        s3_client.upload_file(file_path, bucket_name, object_name)
-        print(f"File uploaded successfully to s3://{bucket_name}/{object_name}")
-    except Exception as e:
-        print(f"Error uploading file: {e}")
+class S3Bucket:
+
+    def __init__(self, file_path: str, bucket_name: str, object_name: str):
+        self.file_path = file_path
+        self.bucket_name = bucket_name
+        self.object_name = object_name
+
+    # Function to upload a file to S3 bucket
+    def upload_file_to_s3(self):
+        try:
+            s3_client.upload_file(self.file_path, self.bucket_name, self.object_name)
+            print(f"File uploaded successfully to s3://{self.bucket_name}/{self.object_name}")
+        except Exception as e:
+            print(f"Error uploading file: {e}")
+
+    def generate_signed_url_for_get(self, expiration=3600):
+        try:
+            signed_url = s3_client.generate_presigned_url(
+                'get_object',
+                Params={'Bucket': self.bucket_name, 'Key': self.object_name},
+                ExpiresIn=expiration
+            )
+            return signed_url
+        except Exception as e:
+            print(f"Error generating URL: {e}")
+
+    def generate_signed_url_for_put(self, expiration=3600):
+        try:
+            signed_url = s3_client.generate_presigned_url(
+                'put_object',
+                Params={'Bucket': self.bucket_name, 'Key': self.object_name},
+                ExpiresIn=expiration,
+                HttpMethod='PUT'
+            )
+            return signed_url
+        except Exception as e:
+            print(f"Error generating URL: {e}")
 
 
-# Function to read a file from S3 bucket
-def read_file_from_s3(bucket_name, object_name):
-    try:
-        response = s3_client.get_object(Bucket=bucket_name, Key=object_name)
-        data = response['Body'].read()
-        print("Data::: ", response)
-        # print(f"Content of file {object_name}:\n{data.decode('utf-8')}")
-    except Exception as e:
-        print(f"Error reading file: {e}")
-
-
-def get_signed_url_for_get(bucket_name, object_name, expiration=3600):
-    try:
-        signed_url = s3_client.generate_presigned_url(
-            'get_object',
-            Params={'Bucket': bucket_name, 'Key': object_name},
-            ExpiresIn=expiration
-        )
-        return signed_url
-    except Exception as e:
-        print(f"Error generating URL: {e}")
-
-
-def get_signed_url_for_put(bucket_name, object_name, expiration=3600):
-    try:
-        signed_url = s3_client.generate_presigned_url(
-            'put_object',
-            Params={'Bucket': bucket_name, 'Key': object_name},
-            ExpiresIn=expiration,
-            HttpMethod='PUT'
-        )
-        return signed_url
-    except Exception as e:
-        print(f"Error generating URL: {e}")
-
-
-# Example usage
-if __name__ == "__main__":
-    # Replace these with your own values
-    file_path = 'path/to/your/local/file.txt'
+def main():
+    file_path = ''
     bucket_name = 'boineer-dev-bucket'
     object_name = 'media/avatar/image.jpg'
 
-    # Upload file to S3
-    # upload_file_to_s3(file_path, bucket_name, object_name)
+    s3_bucket = S3Bucket(
+        bucket_name=bucket_name,
+        object_name=object_name,
+        file_path=file_path
+    )
 
-    # Read file from S3
-    # read_file_from_s3(bucket_name, object_name)
-    # signed_url = get_signed_url_for_get(bucket_name, object_name)
-    signed_url = get_signed_url_for_put(bucket_name, object_name)
+    signed_url = s3_bucket.generate_signed_url_for_get()
+    # signed_url = s3_bucket.generate_signed_url_for_put()
     print(f"URL for the file: {signed_url}")
+
+
+if __name__ == "__main__":
+    main()
